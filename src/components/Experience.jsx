6e19720dd8abc9dd3163a5d5ee3257b86543206c
Niket <shahniket643@gmail.com>
2025-01-29 21:35:45 +0530

    Final push, all functionalities implemented

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -2,13 +2,19 @@ import { Environment, OrbitControls, useTexture } from "@react-three/drei";
 import { Avatar } from "./Avatar";
 import { useThree } from "@react-three/fiber";
 
-export const Experience = () => {
-  const texture = useTexture("textures/nirma-classroom.jpg")
-  const viewport = useThree((state) => state.viewport)
+export const Experience = ({ avatarAnimation, mouthOpenness }) => {
+  const texture = useTexture("textures/nirma-classroom.jpg");
+  const viewport = useThree((state) => state.viewport);
+
   return (
     <>
       <OrbitControls />
-      <Avatar position={[0, -3, 5]} scale={2} />
+      <Avatar
+        position={[0, -3, 5]}
+        scale={2}
+        animation={avatarAnimation}
+        mouthOpenness={mouthOpenness}
+      />
       <Environment preset="sunset" />
       <mesh>
         <planeGeometry args={[viewport.width * 1.75, viewport.height * 1.75]} />
